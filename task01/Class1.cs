80d6110a870f08d4bd1f6318ee2224416f6e78c1
Andrei Leontev <andrei.leontev100620@gmail.com>
2025-06-26 18:36:12 +0600

    Решение первого задания + Добавлен CI

---
+++ task01/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string str)
+    {
+        str = str.ToLower();
+        if (String.IsNullOrEmpty(str))
+        {
+            return false;
+        }
+        string newStr = "";
+        foreach (char c in str)
+        {
+            if (!Char.IsPunctuation(c) && !Char.IsWhiteSpace(c))
+            {
+                newStr += c;
+            }
+        }
+        return newStr.SequenceEqual(newStr.Reverse());
+    }
+}
