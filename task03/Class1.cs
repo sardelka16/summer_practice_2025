d23ebb40d96a2690790162f1041361fecefd9981
Andrei Leontev <andrei.leontev100620@gmail.com>
2025-06-27 22:22:54 +0600

    Выполнил задание 3: Дописал методы в классе CustomCollection, добавлен CI-файл

---
+++ task03/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace task03;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CustomCollection<T> : IEnumerable<T>
+{
+    private readonly List<T> _items = new();
+
+    public void Add(T item) => _items.Add(item);
+
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public IEnumerable<T> GetReverseEnumerator()
+    {
+        for (int i = _items.Count - 1; i >= 0; i--)
+        {
+            yield return _items[i];
+        }
+    }
+
+    public IEnumerable<int> GenerateSequence(int start, int count)
+    {
+        for (int i = start; i < start + count; i++)
+        {
+            yield return i;
+        }
+    }
+
+    public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+    {
+        return _items.Where(predicate).OrderBy(keySelector);
+    }
+}
