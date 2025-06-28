c371e7dd6ad2725a644f22ad6773bf68dc80d061
Andrei Leontev <andrei.leontev100620@gmail.com>
2025-06-28 22:05:26 +0600

    Выполнил задание 5, написал реализацию методов классов с помощью рефлексии, дописал тесты и тестовый класс, добавлен CI-файл

---
+++ task05/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace task05;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public class ClassAnalyzer
+{
+    private readonly Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()
+    {
+        return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
+                    .Select(m => m.Name);
+    }
+    public IEnumerable<string> GetMethodParams(string methodName)
+    {
+        var method = _type.GetMethod(methodName, BindingFlags.Public | BindingFlags.NonPublic |
+                                           BindingFlags.Instance | BindingFlags.Static);
+        return method!.GetParameters().Select(p => p.Name!);
+    }
+
+    public IEnumerable<string> GetAllFields()
+    {
+        return _type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
+                    .Select(f => f.Name);
+    }
+    public IEnumerable<string> GetProperties()
+    {
+        return _type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                    .Select(p => p.Name);
+    }
+    public bool HasAttribute<T>() where T : Attribute
+    {
+        return _type.GetCustomAttributes(typeof(T), false).Any();
+    }
+}
