bb27fcd07c90126e27d2fec5a1f4f1c213b6fccf
Andrei Leontev <andrei.leontev100620@gmail.com>
2025-07-17 19:54:03 +0600

    Выполнено задание 14, исправлены тесты, добавлен CI

---
+++ task14/Class1.cs
@@ -0,0 +1,72 @@
+﻿namespace task14;
+
+using System;
+using System.Threading;
+
+public class ThreadData
+{
+    public Func<double, double> Function;
+    public double Start;
+    public double End;
+    public double Step;
+    public double PartialResult;
+}
+public class DefiniteIntegral
+{
+    private static double totalResult = 0.0;
+    private static readonly object lockObj = new object();
+    private static Barrier barrier;
+
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
+    {
+        totalResult = 0.0;
+        barrier = new Barrier(threadsNumber + 1);
+
+        double intervalLength = (b - a) / threadsNumber;
+        Thread[] threads = new Thread[threadsNumber];
+
+        for (int i = 0; i < threadsNumber; i++)
+        {
+            double start = a + i * intervalLength;
+            double end = (i == threadsNumber - 1) ? b : start + intervalLength;
+
+            ThreadData data = new ThreadData
+            {
+                Function = function,
+                Start = start,
+                End = end,
+                Step = step
+            };
+
+            threads[i] = new Thread(ComputePartialIntegral);
+            threads[i].Start(data);
+        }
+
+        barrier.SignalAndWait();
+        return totalResult;
+    }
+
+    private static void ComputePartialIntegral(object data)
+    {
+        ThreadData threadData = (ThreadData)data;
+        double sum = 0.0;
+        double x = threadData.Start;
+        double end = threadData.End;
+
+        while (x < end)
+        {
+            double nextX = Math.Min(x + threadData.Step, end);
+            sum += (threadData.Function(x) + threadData.Function(nextX)) * (nextX - x) / 2;
+            x = nextX;
+        }
+
+        threadData.PartialResult = sum;
+
+        lock (lockObj)
+        {
+            totalResult += sum;
+        }
+
+        barrier.SignalAndWait();
+    }
+}
---
+++ task14tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿namespace task14tests;
+
+using task14;
+using Xunit;
+public class DefiniteIntegralTests
+{
+    [Fact]
+    public void Solve_LinearFunction_ShouldReturnCorrectAnswers()
+    {
+        var X = (double x) => x;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+
+    [Fact]
+    public void Solve_SinysFunction_ShouldReturnCorrectAnswer()
+    {
+        var SIN = (double x) => Math.Sin(x);
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+}
